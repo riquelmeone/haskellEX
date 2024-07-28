9bf8fa7f109e41d4ec250e4f9519d0dfa5e71be2
Ricardo <ricardo.galan@gmx.net>
2024-07-28 16:48:53 +0200

    feat: added CORS support

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ module Main (main) where
 import Lib
 
 import Web.Scotty
+import Network.Wai.Middleware.Cors (simpleCors)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TE
@@ -11,8 +12,11 @@ import qualified Data.Text.Lazy.Encoding as TE
 
 main :: IO ()
 main = scotty 3000 $ do
+    middleware simpleCors
+
     get "/text" $ do
         html "<h1>Lorem ipsum</h1>"
+
     post "/echo" $ do
         b <- body
         html $ TL.concat ["<h1>POST echo: ", TE.decodeUtf8 b, "</h1>"]
