be6b042aee26e97a7e74db45c79c8af016ad9e45
Ricardo <ricardo.galan@gmx.net>
2024-07-28 16:42:37 +0200

    fix: syntax error in main

--- app/Main.hs
+++ app/Main.hs
@@ -8,13 +8,11 @@ import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TE
 
-f :: 
 
 main :: IO ()
 main = scotty 3000 $ do
     get "/text" $ do
         html "<h1>Lorem ipsum</h1>"
     post "/echo" $ do
-        body <- body
-        let bodyText = TE.decodeUtf8 (BL.toStrict body)
-        text bodyText
+        b <- body
+        html $ TL.concat ["<h1>POST echo: ", TE.decodeUtf8 b, "</h1>"]
