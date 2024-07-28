681d62044c4ae5e836d973fde0b97b4bbe42124f
Ricardo <ricardo.galan@gmx.net>
2024-07-28 20:19:52 +0200

    feat: added html example output for first get request

--- app/Main.hs
+++ app/Main.hs
@@ -8,15 +8,22 @@ import Network.Wai.Middleware.Cors (simpleCors)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TE
+import Control.Monad.IO.Class
 
 
 main :: IO ()
 main = scotty 3000 $ do
-    middleware simpleCors
+    middleware simpleCors   -- CORS support
 
     get "/text" $ do
-        html "<h1>Lorem ipsum</h1>"
+        content <- liftIO $ readFile "frontend/example.html"
+        html $ TL.pack content
+        
+    get "/result" $ do
+        let r = f 18
+        html $ toText r
 
     post "/echo" $ do
         b <- body
         html $ TL.concat ["<h1>POST echo: ", TE.decodeUtf8 b, "</h1>"]
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib
-    ( twoSum, myId, f
+    ( twoSum, myId, f, toText
     ) where
+      
+import qualified Data.Text.Lazy as TL
 
 twoSum :: [Int] -> Int -> (Int, Int)
 twoSum nums target =
@@ -9,10 +11,11 @@ twoSum nums target =
                   i < j,
                   x + y == target]
 
-
 f :: Int -> (Int, Int)
 f x = twoSum [2, 7, 11, 15] x
 
+toText :: (Int, Int) -> TL.Text
+toText = TL.pack . show
 
 myId :: Int -> Int
 myId a = a
