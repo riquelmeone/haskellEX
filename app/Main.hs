4665c63b2a3706869a0142401a64aa4d9db41bc3
Galan Heidenreich <ricardo.galan-heidenreich@materna.group>
2024-08-16 16:42:50 +0200

    feat: added endpoints for Square and Rectangle

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,6 @@ module Main (main) where
 import Lib
 import Shapes
 
-
 import Web.Scotty
 import Network.Wai.Middleware.Cors (simpleCors)
 import qualified Data.Text.Lazy as TL
@@ -28,13 +27,32 @@ main = scotty 3000 $ do
         let result = take n fibList
         html $ toText result
         
-     -- http://localhost:3000/circle/4.5
+    -- http://localhost:3000/circle/4.5
     get "/circle/:f" $ do
-        fText <- pathParam "f"  -- Extracting the parameter as Text
-        let f = read (TL.unpack fText) :: Float  -- Convert Text to Float
+        fText <- pathParam "f"
+        let f = read (TL.unpack fText) :: Float
         let shape = Circle f
         let result = describeShape shape
         html $ toText result
+
+    -- http://localhost:3000/circle/4.5
+    get "/square/:s" $ do
+        sText <- pathParam "s"
+        let s = read (TL.unpack fText) :: Float
+        let shape = Square s
+        let result = describeShape shape
+        html $ toText result
+
+    -- http://localhost:3000/rectangle/2.6/6.2
+    get "/rectangle/:f/:d" $ do
+        fText <- pathParam "f"
+        dText <- pathParam "d"
+        let f = read (TL.unpack fText) :: Float
+        let d = read (TL.unpack dText) :: Float
+        let shape = Rectangle f d
+        let result = describeShape shape
+        html $ toText result
+
 
     -- http://localhost:3000/twosum?nums=1,2,3,4,7,11,15&target=18
     get "/twosum" $ do
@@ -53,4 +71,3 @@ main = scotty 3000 $ do
     post "/echo" $ do
         b <- body
         html $ TL.concat ["POST echo: ", TE.decodeUtf8 b]
-
